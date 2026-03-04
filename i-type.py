3b8016104da969cf49ecb1ab0fd3bf76de752fea
Deepta <deepta25170@iiitd.ac.in>
2026-03-04 17:15:20 +0000

    Added function to convert imm value to binary

--- i-type.py
+++ i-type.py
@@ -7,14 +7,22 @@ instr = { "lw" :["010","0000011"], #0-index: funct3,  1-index: opcode
 # Load instructions to program
 S = []
 def load(): 
-        with open("assembly.txt",'r') as fin:
+        '''with open("assembly.txt",'r') as fin:
             L = fin.readlines()
             for i in L:
                 j = i.replace(",","").split()
-                S.append(j)
-            print(S)    
+                S.append(j)'''
+        L = "addi x2, x3, 10"
+        j = L.replace(",","").split()
+        S.append(j)
+        print(S)    
 load()
 
+#Function to convert immediate value to binary
+def imm_to_bin(imm):
+    return format(int(imm),"012b")
+            
+     
 
               
 
