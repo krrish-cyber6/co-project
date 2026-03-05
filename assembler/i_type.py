687ccd3d29f3ba942ba36d7c8f025f282c6eeb36
Deepta <deepta25170@iiitd.ac.in>
2026-03-05 13:31:39 +0000

    Added error handling

--- assembler/i_type.py
+++ assembler/i_type.py
@@ -1,69 +1,49 @@
-#def itype(command):
+from store import Register_Mapping
+
 instr = { "lw" :["010","0000011"], #0-index: funct3,  1-index: opcode
          "addi" :["000","0010011"],
          "sltiu":["011","0010011"],
          "jalr" :["000","1100111"]  }
 
-# Load instructions to program
-S = []
-def load(): 
-        '''with open("assembly.txt",'r') as fin:
-            L = fin.readlines()
-            for i in L:
-                j = i.replace(",","").split()
-                S.append(j)'''
-        L = "addi x5, x3, -1"
-        j = L.replace(",","").split()
-        S.append(j)
-        print(S)    
-load()
+
+inp = "addi x5, x3, -1"
 
 #Function to convert immediate value to binary
-def imm_to_bin(imm):
-    return format(int(imm),"012b")
+'''def imm_to_bin(imm):
+    imm = int(imm)
+    if imm >= 0:
+        return format(imm, "012b")
+    else:
+        return format( ~imm + 1, "012b")''''
 
 #Function to convert register value to binary            
 def reg_to_bin(reg):
-    return format(int(reg), "05b")            
-def twoComp(imm):
-     
+    return format(int(reg), "05b")     
           
 #Function to convert instructions to binary     
-def toBinary(S):
-    for x in S:
-        b = instr.get(x[0])
-        funct3, opcode = b
+def itoBinary(inp):
+    cmd = []
+    cmd.append(inp.replace(",","").split())
+    try:
+        for x in cmd:
+            b = instr.get(x[0])
+            funct3, opcode = b     
+            if x[0] == "lw":
 
-        #CHECKS FOR IMM
-        if imm < 0:
-            imm  = twoComp(imm)
-        #elif len(imm) > 12:
-            #      raise error   
-        elif imm == []:
-            print("Syntax error")
-        else:
-             #go to label     
-
-        #CHECKS FOR REG
-        if rs1 == [0]:
-            print("x0 being used to store value!")
-            
-
-        if x[0] == "lw":
-
-            rd = reg_to_bin(x[1][1:])   
-            imm, rs1 = x[2].replace(")", "").split("(") #Removing brackets 
-            rs = reg_to_bin(rs1[1:])    
-            imm = imm_to_bin(imm)
-        else:
-            rd = reg_to_bin(x[1][1:])
-            rs = reg_to_bin(x[2][1:])
-            imm = imm_to_bin(x[3])
-    print(imm + rs + funct3 + rd + opcode)
-
-toBinary(S)          
-
-
+                rd = reg_to_bin(x[1][1:])   
+                imm, rs1 = x[2].replace(")", "").split("(") #Removing brackets 
+                rs = reg_to_bin(rs1[1:])    
+                imm = imm_to_bin(imm)
+        
+            else:
+                rd = reg_to_bin(x[1][1:])
+                rs = reg_to_bin(x[2][1:])
+                imm = imm_to_bin(x[3])
+                
+        print(imm + rs + funct3 + rd + opcode)
+    except:
+        raise Exception("Invalid assembly command")
+itoBinary("addi x3, x2, -3")          
 
                       
                         
