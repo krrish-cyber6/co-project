ee2721bfa2e6def6dc5a3061ef6e1ba8df6e73bb
Deepta <deepta25170@iiitd.ac.in>
2026-03-05 16:09:39 +0000

    Updated i_type.py

--- assembler/i_type.py
+++ assembler/i_type.py
@@ -9,16 +9,16 @@ instr = { "lw" :["010","0000011"], #0-index: funct3,  1-index: opcode
 inp = "addi x5, x3, -1"
 
 #Function to convert immediate value to binary
-'''def imm_to_bin(imm):
+def imm_to_bin(imm):
     imm = int(imm)
     if imm >= 0:
         return format(imm, "012b")
     else:
-        return format( ~imm + 1, "012b")''''
+        return format( ~imm + 1, "012b")
 
 #Function to convert register value to binary            
 def reg_to_bin(reg):
-    return format(int(reg), "05b")     
+    return format(Register_Mapping[reg], "05b")
           
 #Function to convert instructions to binary     
 def itoBinary(inp):
@@ -28,22 +28,22 @@ def itoBinary(inp):
         for x in cmd:
             b = instr.get(x[0])
             funct3, opcode = b     
-            if x[0] == "lw":
 
-                rd = reg_to_bin(x[1][1:])   
+            if x[0] == "lw":
+                rd = reg_to_bin(x[1])   
                 imm, rs1 = x[2].replace(")", "").split("(") #Removing brackets 
-                rs = reg_to_bin(rs1[1:])    
+                rs = reg_to_bin(rs1[1])    
                 imm = imm_to_bin(imm)
         
             else:
-                rd = reg_to_bin(x[1][1:])
-                rs = reg_to_bin(x[2][1:])
+                rd = reg_to_bin(x[1])
+                rs = reg_to_bin(x[2])
                 imm = imm_to_bin(x[3])
                 
         print(imm + rs + funct3 + rd + opcode)
     except:
         raise Exception("Invalid assembly command")
-itoBinary("addi x3, x2, -3")          
+itoBinary(inp)          
 
                       
                         
