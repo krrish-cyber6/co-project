c08d8eb486cd60930301c8ed75037c2f5cb88d73
lakshya25293 <lakshya25293@iiitd.ac.in>
2026-03-07 00:12:48 +0530

    Change done in B_Type imm

--- assembler/b_type.py
+++ assembler/b_type.py
@@ -7,8 +7,7 @@ def b_type_encoder(func,r1,r2,off_val,pc,labels):
     else:
         offset=labels[off_val]-pc
     offset = offset >> 1
-    if offset<0:
-        offset = offset % (1 << 12)
+    offset = offset & 0xFFF
     imm=format(offset,"012b")
     imm_12=imm[0]
     imm10_5=imm[2:8]
