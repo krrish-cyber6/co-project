5bcf4853274208ff8fdc1a932e1bf7d750078e10
krrish-cyber6 <krrish25276@iiitd.ac.in>
2026-03-28 19:01:50 +0000

    completed r type

--- simulator/r_type.py
+++ simulator/r_type.py
@@ -1,1 +1,37 @@
-git ad
+def signed(n):
+    temp=format(n,"032b")
+    if temp[0]=="1":
+        return n-2**32
+    else:
+        return n
+
+def rbin_op(bin_instruction, registers):
+    funct7 = bin_instruction[0:7]
+    rs2 = bin_instruction[7:12]
+    rs1 = bin_instruction[12:17]
+    funct3 = bin_instruction[17:20]
+    rd = bin_instruction[20:25]
+    opcode = bin_instruction[25:]
+    if funct7=="0100000" and funct3!="000":
+        raise KeyError
+    elif funct7=="0100000":
+        registers[rd]=(registers[rs1]-registers[rs2])&0xFFFFFFFF
+    elif funct7=="0000000":
+        if funct3=="000":
+            registers[rd]=(registers[rs1]+registers[rs2])&0xFFFFFFFF
+        elif funct3=="001":
+            registers[rd]=((registers[rs1]&0xFFFFFFFF)<<(registers[rs2]&31))&0xFFFFFFFF
+        elif funct3=="010":
+            registers[rd]=(int(signed(registers[rs1])<signed(registers[rs2])))&0xFFFFFFFF
+        elif funct3=="011":
+            registers[rd]=int((registers[rs1])<(registers[rs2]))&0xFFFFFFFF
+        elif funct3=="100":
+            registers[rd]=(registers[rs1]^registers[rs2])&0xFFFFFFFF
+        elif funct3=="101":
+            registers[rd]=((registers[rs1]&0xFFFFFFFF)>>(registers[rs2]&31))&0xFFFFFFFF
+        elif funct3=="110":
+            registers[rd]=(registers[rs1]|registers[rs2])&0xFFFFFFFF
+        elif funct3=="111":
+            registers[rd]=(registers[rs1]&registers[rs2])&0xFFFFFFFF
+        else :
+            raise KeyError
