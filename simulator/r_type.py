c26d50bc901b514a79ef0e1e6f6be2cf6164067e
krrish-cyber6 <krrish25276@iiitd.ac.in>
2026-03-28 18:56:28 +0000

    completed r type

--- simulator/r_type.py
+++ simulator/r_type.py
@@ -1,8 +1,1 @@
-def rbin_op(bin_instruction, registers):
-    funct7 = bin_instruction[0:7]
-    rs1 = [bin_instruction[7:12]]
-    rs2 = bin_instruction[12:17]
-    funct3 = bin_instruction[17:20]
-    rd = bin_instruction[20:25]
-    opcode = bin_instruction[25:]
-    
+git ad
