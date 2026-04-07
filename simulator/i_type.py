c92ebc2e3eb70c199db0be3198367129c2196e6e
Deepta <deepta25170@iiitd.ac.in>
2026-04-07 09:52:42 +0000

    completed addi

--- simulator/i_type.py
+++ simulator/i_type.py
@@ -7,17 +7,18 @@ from simulator.py import registers
 [6:0] = opcode
 '''
 def sext(imm):
-    
+    return imm[0]*20 + imm[0:12]
 
-def sim_i(asm_ins):
-    imm = int(asm_ins[20:32])
-    rs1 = int(asm_ins[15:20])
+def i_type(asm_ins):
+    b_imm = asm_ins[20:32]
+    imm = int(asm_ins[20:32],2)
+    rs1 = int(asm_ins[15:20],2)
     funct3 = asm_ins[12:15]
-    rd = int(asm_ins[7:12])
+    rd = int(asm_ins[7:12],2)
     opcode = asm_ins[0:7]
 
     if funct3 == "000" and opcode == "0010011": #addi
-        registers[rd] = format(imm + registers[rs1],"032b")
+        registers[rd] = format((int(sext(b_imm),2) + int(registers[rs1],2))&0xFFFFFFFF,"032b")
 
 
         
